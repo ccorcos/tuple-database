af4ec9b60e972fabac147222e93086a953ea15d7
Chet Corcos <ccorcos@gmail.com>
2020-12-20 18:50:42 -0800

    Cleanup and comment:

--- example/preload.js
+++ example/preload.js
@@ -1,38 +1,25 @@
+// Please see the README.md for instructions on how to run this example.
 const { FileStorage } = require("../build/database/FileStorage")
 const { ReactiveStorage } = require("../build/database/ReactiveStorage")
 const { MIN, MAX } = require("../build/database/types")
 const _ = require("lodash")
 
+// Initialize a database.
 const dbPath = __dirname + "/example.db"
 const db = new ReactiveStorage(new FileStorage(dbPath))
 
-// =========================================================================
-
+// I don't want to setup a whole build system for this example, so I'm going
+// to be doing everything here in the preload script so that `require` is available.
 const ReactDOM = require("react-dom")
 const React = require("react")
 const { useEffect, useMemo, useCallback } = require("react")
 
+// I don't want to setup and JSX preprocessing, so I'll be doing it the old-school way.
 const h = React.createElement
 
-function useDeepEqual(obj) {
-	const prev = React.useRef()
-	React.useEffect(() => {
-		prev.current = obj
-	}, [obj])
-
-	const same = _.isEqual(prev.current, obj)
-	const id = React.useRef(Math.random())
-	if (same) {
-		return id.current
-	} else {
-		id.current = Math.random()
-		return id.current
-	}
-}
-
+// React hook for a reactive db query.
 function useSubscribe(index, args) {
 	const [state, setState] = React.useState([])
-
 	const argsDep = useDeepEqual(args)
 
 	useEffect(() => {
@@ -49,31 +36,29 @@ function useSubscribe(index, args) {
 }
 
 // Write to both indexes.
-function writeTodo(todo) {
-	db.transact()
-		.set("todos", [-todo.time, todo])
-		.set("todos-complete", [todo.completed, -todo.time, todo])
-		.commit()
+function writeTodo(tx, todo) {
+	tx.set("todos", [-todo.time, todo]).set("todos-complete", [
+		todo.completed,
+		-todo.time,
+		todo,
+	])
 }
 
-// TODO: compose together write and remove logic.
-function updateTodo(oldTodo, todo) {
-	db.transact()
-		.remove("todos", [-oldTodo.time, oldTodo])
-		.remove("todos-complete", [oldTodo.completed, -oldTodo.time, oldTodo])
-		.set("todos", [-todo.time, todo])
-		.set("todos-complete", [todo.completed, -todo.time, todo])
-		.commit()
+function deleteTodo(tx, oldTodo) {
+	tx.remove("todos", [-oldTodo.time, oldTodo]).remove("todos-complete", [
+		oldTodo.completed,
+		-oldTodo.time,
+		oldTodo,
+	])
 }
 
-function deleteTodo(oldTodo) {
-	db.transact()
-		.remove("todos", [-oldTodo.time, oldTodo])
-		.remove("todos-complete", [oldTodo.completed, -oldTodo.time, oldTodo])
-		.commit()
+function updateTodo(tx, oldTodo, todo) {
+	deleteTodo(tx, oldTodo)
+	writeTodo(tx, todo)
 }
 
 function App() {
+	// Create a new todo item.
 	const handleClick = React.useCallback(() => {
 		const todo = {
 			id: Math.random(),
@@ -81,23 +66,28 @@ function App() {
 			title: "",
 			completed: false,
 		}
-		writeTodo(todo)
+		const tx = db.transact()
+		writeTodo(tx, todo)
+		tx.commit()
 	}, [])
 
-	const [state, setState] = React.useState("all") // "all" | "completed" | "not-completed"
+	// The todos filter.
+	const [filter, setFilter] = React.useState("all") // "all" | "completed" | "not-completed"
 
 	const onChangeFilter = useCallback((e) => {
-		setState(e.target.value)
+		setFilter(e.target.value)
 	}, [])
 
-	const index = state === "all" ? "todos" : "todos-complete"
+	// Determine which index to use and which scan args to use.
+	const index = filter === "all" ? "todos" : "todos-complete"
 	const args =
-		state === "all"
+		filter === "all"
 			? {}
-			: state === "completed"
+			: filter === "completed"
 			? { gte: [true, MIN], lte: [true, MAX] }
 			: { gte: [false, MIN], lte: [false, MAX] }
 
+	// Register the subscription and get the todo object from the last element of the tuple.
 	const todos = useSubscribe(index, args).map(
 		(tuple) => tuple[tuple.length - 1]
 	)
@@ -105,81 +95,109 @@ function App() {
 	return h(
 		"div",
 		{ style: { margin: "3em auto", maxWidth: "20em" } },
+
+		// New Todo button.
 		h(
 			"button",
 			{ onClick: handleClick, style: { margin: "1em 1.5em" } },
 			"New To-do"
 		),
 
-		// Filters
+		// Todo Filters
 		h(
 			"div",
 			{},
 			h("input", {
 				type: "radio",
 				value: "all",
-				checked: state === "all",
+				checked: filter === "all",
 				onChange: onChangeFilter,
 			}),
 			"All"
 		),
-
 		h(
 			"div",
 			{},
 			h("input", {
 				type: "radio",
 				value: "completed",
-				checked: state === "completed",
+				checked: filter === "completed",
 				onChange: onChangeFilter,
 			}),
 			"Completed"
 		),
-
 		h(
 			"div",
 			{ style: { marginBottom: "1em" } },
 			h("input", {
 				type: "radio",
 				value: "not-completed",
-				checked: state === "not-completed",
+				checked: filter === "not-completed",
 				onChange: onChangeFilter,
 			}),
 			"Not Completed"
 		),
 
+		// Render the todos.
 		...todos.map((todo) => {
 			const onChange = (e) => {
-				updateTodo(todo, { ...todo, title: e.target.value })
+				const tx = db.transact()
+				updateTodo(tx, todo, { ...todo, title: e.target.value })
+				tx.commit()
 			}
 
 			const onToggle = (e) => {
-				updateTodo(todo, { ...todo, completed: !todo.completed })
+				const tx = db.transact()
+				updateTodo(tx, todo, { ...todo, completed: !todo.completed })
+				tx.commit()
 			}
 
 			const onDelete = (e) => {
-				deleteTodo(todo)
+				const tx = db.transact()
+				deleteTodo(tx, todo)
+				tx.commit()
 			}
 
 			return h(
 				"div",
 				{ key: todo.id },
+
+				// Completed checkbox.
 				h("input", {
 					type: "checkbox",
 					checked: todo.completed,
 					onChange: onToggle,
 				}),
+
+				// Todo title input.
 				h("input", { value: todo.title, onChange }),
+
+				// Delete todo button.
 				h("button", { onClick: onDelete, style: { marginLeft: "0.5em" } }, "x")
 			)
 		})
 	)
 }
 
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+// Initial render.
 window.addEventListener("DOMContentLoaded", () => {
 	const div = document.getElementById("root")
-
 	ReactDOM.render(h(App), div)
 })
+
+// Nitty-gritty details for React-hooks.
+function useDeepEqual(obj) {
+	const prev = React.useRef()
+	React.useEffect(() => {
+		prev.current = obj
+	}, [obj])
+
+	const same = _.isEqual(prev.current, obj)
+	const id = React.useRef(Math.random())
+	if (same) {
+		return id.current
+	} else {
+		id.current = Math.random()
+		return id.current
+	}
+}
