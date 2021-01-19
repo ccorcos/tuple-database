850ff8eb91b36d30aa3b7ccc5290444003d1ff5c
Chet Corcos <ccorcos@gmail.com>
2021-01-18 16:10:13 -0800

    Encoder class

--- src/helpers/codec.ts
+++ src/helpers/codec.ts
@@ -1,9 +1,42 @@
 // This codec is should create a component-wise lexicographically sortable array.
 
-import { invert } from "lodash"
+import { invert, isPlainObject } from "lodash"
 import * as elen from "elen"
 import { MIN, MAX, Value, Tuple } from "../storage/types"
 import { compare } from "./compare"
+
+export type Encoding<T> = {
+	byte: string
+	is(value: unknown): boolean
+	encode(value: T): string
+	decode(value: string): T
+}
+
+export class Encoder<T> {
+	private encodingList: Array<Encoding<T>> = []
+	private encodingMap: Record<string, Encoding<T>> = {}
+	add<E extends T>(encoding: Encoding<E>) {
+		this.encodingList.push(encoding)
+		this.encodingMap[encoding.byte] = encoding
+	}
+	encode(obj: T): string {
+		for (const encoding of this.encodingList) {
+			if (encoding.is(obj)) {
+				return encoding.byte + encoding.encode(obj)
+			}
+		}
+		throw new Error("Could not encode: " + obj)
+	}
+	decode(str: string): T {
+		const byte = str[0]
+		const rest = str.slice(1)
+		const encoding = this.encodingMap[byte]
+		if (!encoding) {
+			throw new Error("Could find encoding for byte: " + byte)
+		}
+		return encoding.decode(rest)
+	}
+}
 
 // MIN < null < object < array < number < string < boolean < MAX
 const encodeType = {
@@ -16,6 +49,107 @@ const encodeType = {
 	null: "b",
 	MIN: "a",
 } as const
+
+const maxEnc: Encoding<typeof MAX> = {
+	byte: encodeType.MAX,
+	is: (value) => value === MAX,
+	encode(value) {
+		return ""
+	},
+	decode(value) {
+		return MAX
+	},
+}
+
+const minEnc: Encoding<typeof MIN> = {
+	byte: encodeType.MIN,
+	is: (value) => value === MIN,
+	encode(value) {
+		return ""
+	},
+	decode(value) {
+		return MIN
+	},
+}
+
+const nullEnc: Encoding<null> = {
+	byte: encodeType.null,
+	is: (value) => value === null,
+	encode(value) {
+		return ""
+	},
+	decode(value) {
+		return null
+	},
+}
+
+const stringEnv: Encoding<string> = {
+	byte: encodeType.string,
+	is: (value) => typeof value === "string",
+	encode(value) {
+		return value
+	},
+	decode(value) {
+		return value
+	},
+}
+
+const numberEnv: Encoding<number> = {
+	byte: encodeType.number,
+	is: (value) => typeof value === "number",
+	encode(value) {
+		return elen.encode(value)
+	},
+	decode(value) {
+		return elen.decode(value)
+	},
+}
+
+const booleanEnv: Encoding<boolean> = {
+	byte: encodeType.boolean,
+	is: (value) => value === true || value === false,
+	encode(value) {
+		return value.toString()
+	},
+	decode(value) {
+		if (value === "true") {
+			return true
+		}
+		if (value === "false") {
+			return false
+		}
+		throw new Error(`Failed parse boolean: ${value}`)
+	},
+}
+
+const arrayEnv: Encoding<Array<any>> = {
+	byte: encodeType.array,
+	is: (value) => Array.isArray(value),
+	encode(value) {
+		return encodeTuple(value)
+	},
+	decode(value) {
+		return decodeTuple(value)
+	},
+}
+
+const objectEnv: Encoding<{ [key: string]: any }> = {
+	byte: encodeType.object,
+	is: (value) => isPlainObject(value),
+	encode(value) {
+		return encodeObjectValue(value)
+	},
+	decode(value) {
+		return decodeObjectValue(value)
+	},
+}
+
+// array
+// object
+
+// Generalize this so its extensible.
+// - Date -> date type.
+// - Id -> uuid type.
 
 // Generalize this so its extensible.
 // - Date -> date type.
