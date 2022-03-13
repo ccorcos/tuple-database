e0ca68f21972d12e7014f8cd3909e79e4c2cbb36
Chet Corcos <ccorcos@gmail.com>
2022-03-13 03:23:15 -0700

    Almost there.

--- src/storage/tupleTypes.ts
+++ src/storage/tupleTypes.ts
@@ -1,14 +1,5 @@
+import { TupleDatabase } from "../main"
 import { Tuple, TupleValuePair } from "./types"
-
-// | [["eav", string, string, string | number], null]
-// | [["ave", string, string | number, string], null]
-// | [["vea", string | number, string, string], null]
-
-type Schema =
-	| [["link", string, string], null]
-	| [["backlink", string, string], null]
-	| [["recentFiles", "byPath", string, number], null]
-	| [["recentFiles", "byTime", number, string], null]
 
 type Ints = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10"
 // | `${Ints}${Ints}`
@@ -20,31 +11,57 @@ type FilterByPrefix<S extends TupleValuePair, P extends Tuple> = Extract<
 	{ 0: TupleToObject<P> }
 >
 
-// type Z = [] extends { 0: any } ? true : false
+type IsTuple = [] | { 0: any }
 
-type a = TuplePrefix<[1, 2, 3]>
+type TuplePrefix<T extends unknown[]> = T extends IsTuple
+	? T extends [any, ...infer U]
+		? [] | [T[0]] | [T[0], ...TuplePrefix<U>]
+		: []
+	: T
 
-type TuplePrefix<T extends unknown[]> = T extends [any, ...infer U]
-	? [] | [T[0]] | [T[0], ...TuplePrefix<U>]
-	: []
+class SchemaDialect<S extends TupleValuePair = TupleValuePair> {
+	constructor(private db: TupleDatabase, private prefix: any[] = []) {}
 
-class SchemaDialect<S extends TupleValuePair> {
 	scan<P extends TuplePrefix<S[0]>>(args: {
 		prefix?: P
 		gt?: P
 		lt?: P
 	}): FilterByPrefix<S, P>[] {
-		return [] // TODO
+		const prefixedArgs: any = { ...args }
+		if (prefixedArgs.prefix)
+			prefixedArgs.prefix = [...this.prefix, ...prefixedArgs.prefix]
+		if (prefixedArgs.gt) prefixedArgs.gt = [...this.prefix, ...prefixedArgs.gt]
+		if (prefixedArgs.lt) prefixedArgs.lt = [...this.prefix, ...prefixedArgs.lt]
+
+		return this.db.scan(prefixedArgs) as any[]
 	}
 
 	subspace<P extends TuplePrefix<S[0]>>(
 		prefix: P
 	): SchemaDialect<FilterByPrefix<S, P>> {
-		return {} as any // TODO
+		return new SchemaDialect(this.db, [...this.prefix, ...prefix])
 	}
 }
+
+// | [["eav", string, string, string | number], null]
+// | [["ave", string, string | number, string], null]
+// | [["vea", string | number, string, string], null]
+
+type Schema =
+	| [["link", string, string], null]
+	| [["backlink", string, string], null]
+	| [["recentFiles", "byPath", string, number], null]
+	| [["recentFiles", "byTime", number, string], null]
 
 type X = TuplePrefix<Tuple>
 
 declare const x: SchemaDialect<Schema>
 const y = x.scan({ prefix: ["recentFiles"] })
+
+const xxx = x.subspace(["recentFiles"])
+xxx.scan({ prefix: ["byPath"] })
+
+declare const x1: SchemaDialect
+const y1 = x1.scan({ prefix: ["recentFiles"] })
+
+// TODO: what about generic tuples though.
