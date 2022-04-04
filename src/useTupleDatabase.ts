6a360e783a058f1f3b0c259d356f3c8cd3d21852
Chet Corcos <ccorcos@gmail.com>
2022-04-04 16:12:13 -0700

    Include shallow equal for rerender

--- src/useTupleDatabase.ts
+++ src/useTupleDatabase.ts
@@ -1,7 +1,25 @@
+import { intersection, isArray, isPlainObject } from "lodash"
 import { useEffect, useMemo, useRef, useState } from "react"
 import { subscribeQuery } from "./database/sync/subscribeQuery"
 import { TupleDatabaseClientApi } from "./database/sync/types"
 import { KeyValuePair } from "./storage/types"
+
+function shallowEqual(a: any, b: any) {
+	if (a == b) return true
+	if (isArray(a)) {
+		if (!isArray(b)) return false
+		if (a.length !== b.length) return false
+		return a.every((x, i) => b[i] === x)
+	}
+	if (isPlainObject(a)) {
+		if (!isPlainObject(b)) return false
+		const keys = Object.keys(a)
+		const sameKeys = intersection(keys, Object.keys(b))
+		if (keys.length !== sameKeys.length) return false
+		return keys.every((key) => a[key] == b[key])
+	}
+	return false
+}
 
 function useRerender() {
 	const [state, setState] = useState(0)
@@ -22,8 +40,10 @@ export function useTupleDatabase<S extends KeyValuePair, T, A extends any[]>(
 			db,
 			(db) => fn(db, ...args),
 			(result) => {
-				resultRef.current = result
-				rerender()
+				if (!shallowEqual(resultRef.current, result)) {
+					resultRef.current = result
+					rerender()
+				}
 			}
 		)
 		resultRef.current = result
