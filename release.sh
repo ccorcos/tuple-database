3b60de8146a261396af4464369144ab4b91dfdb4
Chet Corcos <ccorcos@gmail.com>
2022-07-28 17:06:05 -0700

    Cleanup release script to fix source maps

--- release.sh
+++ release.sh
@@ -3,10 +3,19 @@ set -e
 # npm version patch
 
 rm -rf build
-npm run build
+npm run build:macros
+
+# Fix tsconfig and compile so that source maps work.
+cp -r src build
+cp tsconfig.json build
+sed -i '' 's#"src/#"#g' build/tsconfig.json
+sed -i '' 's#"build"#"."#g' build/tsconfig.json
+./node_modules/.bin/tsc -p build/tsconfig.json
+
 cp package.json build
 cp .npmignore build
 cp README.md build
+
 
 cd build
 npm publish
